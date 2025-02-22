e64ceb26a93b76b2f737a9eca4f9bb8ff78d7b62
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-22 09:19:04 +0530

    did print 1 to n using IBH

---
+++ 3. Hypothesis-Induction-Base condition/main.cpp
@@ -0,0 +1,109 @@
+/*
+
+flow:
+
+methods to think for a recursive problem
+
+
+why working Input smaller is popular. importanr
+
+print 1 to n
+
+
+4 Approaches
+
+1->recursive tree (when decisions making is clear)
+2->BC-induction-hypothesis (when you just want to make the input smaller)
+3->choice diagram (DP playist)
+4->will do later
+
+recurive tree ki jageh we desgin a hypothesos here
+
+we make up what will the recusive function do
+
+base condition
+    1>smallest valid input
+    2->largest invalid input
+
+IBH is used widely in Linkest list and trees data structure
+
+
+good recursive problems are solves using recursive tree only
+
+
+*/
+
+
+
+//print 1 to n using recusion
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+//hypothesis is function will print from 1 to n
+void print(int n){ 
+    //base case
+    if(n == 1) {
+        cout << n << " ";
+        return;
+    }
+    print(n-1);
+    //induction
+    cout<<n<<" ";
+}
+
+void solve(){
+    int n; cin>>n;
+
+    print(n);
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    
