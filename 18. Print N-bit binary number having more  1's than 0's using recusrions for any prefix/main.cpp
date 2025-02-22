8daf830bbaedb8c0f55ee6aa59c58f9c5c0f1dd6
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-22 15:29:21 +0530

    Print n bit binary number having more 1 than 0 for any prefix using recursion

---
+++ 18. Print N-bit binary number having more  1's than 0's using recusrions for any prefix/main.cpp
@@ -0,0 +1,127 @@
+/*
+correction =>
+
+
+
+*/
+
+
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+
+// void print(int n, string curr, vector<string>& ans, int one, int zero){
+//     if(curr.size() == n) {
+//         ans.push_back(curr);
+//         return;
+//     }
+
+//     string op1;
+//     string op2;
+
+//     if(one<n/2+1){
+//         op1.push_back('1');
+//         print(n, op1, ans, one+1, zero);
+//     }
+//     if(zero<n/2) {
+//         op2.push_back('0');
+//         print(n, op2, ans, one, zero+1);
+//     }
+// }
+
+
+
+// void print(int n, string curr, vector<string>& ans, int one, int zero) {
+//     if (curr.length() == n) {
+//         ans.push_back(curr);
+//         return;
+//     }
+
+//     if (one < n / 2+1) {
+//         print(n, curr + "1", ans, one + 1, zero);
+//     }
+    
+//     if (zero < n / 2) {
+//         print(n, curr + "0", ans, one, zero + 1);
+//     }
+// }
+
+
+
+
+void print(int n, string curr, vector<string>& ans, int one, int zero) {
+    if (curr.length() == n) {
+        if(one>zero){
+            ans.push_back(curr);
+        }
+        return;
+    }
+    //always add 1a one
+    print(n, curr+"1", ans, one+1, zero);
+    
+    //add zero onelu oif one>zero
+    if(one>zero){
+        print(n, curr+"0", ans, one, zero+1);
+    }
+}
+
+void solve() {
+    int n = 6; // Change as needed
+    vector<string> ans;
+    print(n, "", ans, 0, 0);
+
+    for (const auto& str : ans) {
+        cout << str << endl;
+    }
+}
+
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    
---
+++ 18. Print N-bit binary number having more  1's than 0's using recusrions for any prefix/main2.cpp
@@ -0,0 +1,108 @@
+/*
+we have to store all the prefixess such that #1 is #0 for wach of the prefix
+
+*/
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, n) for (int i = (a); i <= (n); i++)
+#define nl cout << "\n"
+#define dbg(var) cout << #var << "=" << var << " "
+#define all(v) v.begin(), v.end()
+#define sz(v) (int)(v.size())
+#define srt(v) sort(v.begin(), v.end())         // sort
+#define mxe(v) *max_element(v.begin(), v.end()) // find max element in vector
+#define mne(v) *min_element(v.begin(), v.end()) // find min element in vector
+#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed
+#define bin(x, y) bitset<y>(x)
+using namespace std;
+int MOD = 1e9 + 7; // Hardcoded, directly change from here for functions!
+
+void modadd(int &a, int b) { a = ((a % MOD) + (b % MOD)) % MOD; }
+void modsub(int &a, int b) { a = ((a % MOD) - (b % MOD) + MOD) % MOD; }
+void modmul(int &a, int b) { a = ((a % MOD) * (b % MOD)) % MOD; }
+// ================================== take ip/op like vector,pairs directly!==================================
+template <typename typC, typename typD>
+istream &operator>>(istream &cin, pair<typC, typD> &a) { return cin >> a.first >> a.second; }
+template <typename typC>
+istream &operator>>(istream &cin, vector<typC> &a)
+{
+    for (auto &x : a)
+        cin >> x;
+    return cin;
+}
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const pair<typC, typD> &a) { return cout << a.first << ' ' << a.second; }
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a)
+{
+    for (auto &x : a)
+        cout << x << '\n';
+    return cout;
+}
+template <typename typC>
+ostream &operator<<(ostream &cout, const vector<typC> &a)
+{
+    int n = a.size();
+    if (!n)
+        return cout;
+    cout << a[0];
+    for (int i = 1; i < n; i++)
+        cout << ' ' << a[i];
+    return cout;
+}
+// ===================================END Of the input module ==========================================
+
+void print(int n, string curr, vector<string> &ans, int one, int zero)
+{
+    // we get our ans ony when the countn of ones for n==0 > zeroes at that moment
+    if (n == 0)
+    {
+        ans.push_back(curr);
+        return;
+    }
+    string op1 = curr + "1";
+    // op1.push_back("1");
+    print(n - 1, op1, ans, one + 1, zero);
+    // print(n - 1, curr + "1", ans, one + 1, zero);
+    if (one > zero)
+    {   
+        string op2 = curr+"0";
+        print(n - 1, op2, ans, one, zero + 1);
+        // print(n - 1, curr + "0", ans, one, zero + 1);
+    }
+}
+
+void solve()
+{
+    int n = 6; // Change as needed
+    vector<string> ans;
+    print(n, "", ans, 0, 0);
+
+    for (const auto &str : ans)
+    {
+        cout << str << endl;
+    }
+}
+
+int32_t main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
