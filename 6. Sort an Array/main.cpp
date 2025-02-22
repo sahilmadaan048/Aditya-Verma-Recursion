086a0237f27f2eec64c999dfca13857b6062f898
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-22 09:51:13 +0530

    sort an array using IBH

---
+++ 6. Sort an Array/main.cpp
@@ -0,0 +1,103 @@
+/*
+we are given an array as input
+nd we have to sort it using
+recursion
+dont worry about the time complexity for ow
+just solve it for the sake of understanding recursion
+
+
+*/
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+//this function inserts the element to its righr place in the array to its right place
+void insertSorted(vi & temp, int val) {
+    //base condition
+    if(temp.empty() || temp.back()<=val) {
+        temp.push_back(val);
+        return;
+    }
+
+    //remove the last element
+    int last = temp.back();
+    temp.pop_back();
+
+    //inducion step
+    insertSorted(temp, val);
+
+    //restrore the removed element
+    temp.push_back(last);
+}
+
+
+//hypitheseis is it returns an sorted array
+void sort(vi &temp){
+    //base condition
+    if(temp.size() == 1){
+        //an array of ;ength 1 is sorted in itself
+        return;
+    }
+    //inducion step
+    int last = temp[temp.size()-1]; //or int last = temp.back();
+    temp.pop_back();
+    sort(temp);
+    insertSorted(temp, last);
+}
+
+void solve(){
+    vi temp = {5, 4, 2, 1, 3};
+    sort(temp);
+    fr(i,temp.size()){
+        cout<<temp[i]<<" ";
+    }
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    
