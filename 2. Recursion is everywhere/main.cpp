a1ac7a02751363b4673e281d1008a7c3b84b5008
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-22 09:06:00 +0530

    recursion is everywhere

---
+++ 2. Recursion is everywhere/main.cpp
@@ -0,0 +1,46 @@
+/*
+array, string
+
+tree
+
+graph (dfs, flood fill)
+
+DP  
+
+Recurion is in itself is a set of recusrive problems
+DP
+Backtracking
+Divide abd Conquer
+
+
+we are doing only the base for recusion here
+
+
+print 1 to n and n to 1
+
+sort an array
+
+delete middle elements of a stack
+
+remove duoplicated from a string
+
+count the number of occurences
+
+subset
+
+permutations 
+    a with spaces 
+    b with case change
+    c and many more
+
+    hosepheus problem
+
+-------- EXTENDED input output  method
+
+(Hard)
+
+Binary string wher #1s > #s
+
+Generate balanced parenthesis
+
+*/
