4fc11d9efd36d3556316f2572ab1fc83b5346879
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-22 13:40:31 +0530

    permutation with case change done using IBH

---
+++ 15. Permutation with coin change/main.cpp
@@ -0,0 +1,96 @@
+/*
+given a input string
+AB
+
+print simethinglike
+ab
+aB
+Ab
+AB
+
+
+for each character there are two choices
+eithe make it lower case or make irt upper case
+
+*/
+
+
+#include "bits/stdc++.h"
+#define int long long
+#define uint unsigned long long
+#define vi vector<int>
+#define vvi vector<vi >
+#define vb vector<bool>
+#define vvb vector<vb >
+#define fr(i,n) for(int i=0; i<(n); i++)
+#define rep(i,a,n) for(int i=(a); i<=(n); i++)
+#define nl cout<<"\n"
+#define dbg(var) cout<<#var<<"="<<var<<" "
+#define all(v) v.begin(),v.end()
+#define sz(v) (int)(v.size())
+#define srt(v)  sort(v.begin(),v.end())         // sort 
+#define mxe(v)  *max_element(v.begin(),v.end())     // find max element in vector
+#define mne(v)  *min_element(v.begin(),v.end())     // find min element in vector
+#define unq(v)  v.resize(distance(v.begin(), unique(v.begin(), v.end())));
+// make sure to sort before applying unique // else only consecutive duplicates would be removed 
+#define bin(x,y)  bitset<y>(x) 
+using namespace std;
+int MOD=1e9+7;      // Hardcoded, directly change from here for functions!
+
+
+
+void modadd(int &a , int b) {a=((a%MOD)+(b%MOD))%MOD;}
+void modsub(int &a , int b) {a=((a%MOD)-(b%MOD)+MOD)%MOD;}
+void modmul(int &a , int b) {a=((a%MOD)*(b%MOD))%MOD;}
+// ================================== take ip/op like vector,pairs directly!==================================
+template<typename typC,typename typD> istream &operator>>(istream &cin,pair<typC,typD> &a) { return cin>>a.first>>a.second; }
+template<typename typC> istream &operator>>(istream &cin,vector<typC> &a) { for (auto &x:a) cin>>x; return cin; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const pair<typC,typD> &a) { return cout<<a.first<<' '<<a.second; }
+template<typename typC,typename typD> ostream &operator<<(ostream &cout,const vector<pair<typC,typD>> &a) { for (auto &x:a) cout<<x<<'\n'; return cout; }
+template<typename typC> ostream &operator<<(ostream &cout,const vector<typC> &a) { int n=a.size(); if (!n) return cout; cout<<a[0]; for (int i=1; i<n; i++) cout<<' '<<a[i]; return cout; }
+// ===================================END Of the input module ==========================================
+
+void print(string ip, string op, vector<string>& ans) {
+    if(ip.size() == 0){
+        ans.push_back(op);
+        return;
+    }
+    //we make it lower vase
+    string op1 = op;
+    op1.push_back(tolower(ip[0]));
+    //we make it upper case
+    string op2 = op;
+    op2.push_back(toupper(ip[0]));
+    ip.erase(ip.begin());
+    print(ip, op1, ans);
+    print(ip, op2, ans);
+}
+
+void solve(){
+    string temp = "ab";
+    vector<string> ans;
+    string curr = "";
+    print(temp, curr, ans);
+
+    for(auto &str: ans) {
+        cout<<str<<endl;
+    }
+    // cout<<endl;
+}
+
+int32_t main()
+{
+ 
+ ios_base::sync_with_stdio(false);
+ cin.tie(NULL);
+
+    int T = 1;
+    // cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
+    
