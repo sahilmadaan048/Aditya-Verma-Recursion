0c506b4a7d50fdd7bffba2fe1e183bbb7be2c765
Sahil Madaan <madaan.sahil27@gmail.com>
2025-02-22 09:00:10 +0530

    done with intro to recursion

---
+++ 1. Recursion intrduction and Identification/main.cpp
@@ -0,0 +1,62 @@
+/*
+flow: 
+
+1. make input smaller but why ?
+
+innput small ho jaat hai we dont do it deliberately
+
+ham kuch kaam karte hain ehich is primtary , kuch decisions lete hain
+unn decisions ki wajeh se input chhota ho jaata hai
+
+ultimately ham decision lene ki koshish kar rahe hain
+
+input apne aap chhote ho jayenge
+
+*/
+
+
+/*
+1. Recursion => decision space
+
+choice + decision
+
+then recursion woll be used
+
+*/
+
+/*
+3. Recursive tree => problem is done
+
+example for subset problem
+
+*/
+
+/*
+
+input output method
+
+we will represent these choices usinf a recursive tree
+
+np of branches  => no of choices
+
+we can do it dfor   o/p    i/p
+                    " "   "ab" 
+
+                "" "b"  ,    "a" "b"
+          
+      "" "" ,  "b" ""       "a" "" , "ab" "" 
+
+
+we get our ans when the inout string bwcomes empty ""
+
+this is how the flowo will work
+
+*/
+
+/*
+desgin a recursive tree
+
+fuck the problem
+write the code basically
+
+*/
